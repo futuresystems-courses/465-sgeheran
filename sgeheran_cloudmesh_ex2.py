b0e34840a8625ab1c09fc11f7aac1e5673abf480
Sean Geheran <sean.geheran@ge.com>
2015-05-11 05:35:33 -0400

    updated script

--- sgeheran_cloudmesh_ex2.py
+++ sgeheran_cloudmesh_ex2.py
@@ -1,13 +1,13 @@
-sgeheran
-+--------------+--------+
-| cloud        | active |
-+--------------+--------+
-| aws          |        |
-| azure        |        |
-| devstack     |        |
-| dreamhost    |        |
-| hp           |        |
-| hp_east      |        |
-| india        | True   |
-| india_havana |        |
-+--------------+--------+
+(ENV)ubuntu@sgeheran-sgeheran-ta-cherrypy:~$ ipython
+Python 2.7.6 (default, Mar 22 2014, 22:59:56)
+Type "copyright", "credits" or "license" for more information.
+
+IPython 3.1.0 -- An enhanced Interactive Python.
+?         -> Introduction and overview of IPython's features.
+%quickref -> Quick reference.
+help      -> Python's own help system.
+object?   -> Details about 'object', use 'object??' for extra details.
+
+In [1]:  import cloudmesh
+
+In [2]: cloudmesh.shell("cloud list")
