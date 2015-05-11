dfa30148b071bb6c55c84d8f3a41e7e818c901e8
Sean Geheran <sean.geheran@ge.com>
2015-05-11 05:44:46 -0400

    updates made

--- sgeheran_cloudmesh_ex3.py
+++ sgeheran_cloudmesh_ex3.py
@@ -1,1 +1,8 @@
+In [3]: print cloudmesh.shell("cloud on india")
+sgeheran
+cloud 'india' activated.
 
+
+In [4]: vmname = "sgeheran_cloudmesh_ex3"
+
+In [5]: print cloudmesh.shell("vm start --cloud=india --image=futuresystems/ubuntu-14.04 --flavor=m1.small --name={0}".format(vmname))
